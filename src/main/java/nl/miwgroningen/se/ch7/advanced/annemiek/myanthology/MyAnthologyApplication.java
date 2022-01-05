a067ec7b4b5bdbe6f6caa7b022b36951ca9ce3e1
Annemiek Blaauwgeers <a.blaauwgeers@st.hanze.nl>
2022-01-05 09:47:45 +0100

    Initial commit

---
+++ src/main/java/nl/miwgroningen/se/ch7/advanced/annemiek/myanthology/MyAnthologyApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se.ch7.advanced.annemiek.myanthology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnthologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnthologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/se/ch7/advanced/annemiek/myanthology/MyAnthologyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se.ch7.advanced.annemiek.myanthology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnthologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
