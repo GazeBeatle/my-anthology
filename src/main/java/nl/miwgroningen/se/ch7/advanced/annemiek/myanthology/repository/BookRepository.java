708132a36e3424b4e3dc8843ca204ef67e1cfcc5
Annemiek Blaauwgeers <a.blaauwgeers@st.hanze.nl>
2022-01-08 11:18:23 +0100

    Find Books by reading status, edited Overview to show read/unread books

--- src/main/java/nl/miwgroningen/se/ch7/advanced/annemiek/myanthology/repository/BookRepository.java
+++ src/main/java/nl/miwgroningen/se/ch7/advanced/annemiek/myanthology/repository/BookRepository.java
@@ -3,8 +3,11 @@ package nl.miwgroningen.se.ch7.advanced.annemiek.myanthology.repository;
 import nl.miwgroningen.se.ch7.advanced.annemiek.myanthology.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByTitle(String title);
+
+    List<Book> findByisRead(boolean isRead);
 }
